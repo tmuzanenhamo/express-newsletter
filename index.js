7b9def2970e8143de74dcb6a9849efcc66bb0330
tmuzanenhamo <tawandamuzanenhamo14@gmail.com>
2020-11-22 00:28:29 +0200

    Added Mail Chimp Integration

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app = express()
-const PORT = 3000;
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extened: true}))
+const app = express(); // init the application
 
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname+'/signup.html');
-})
+// Extract env variables from the .env file
+const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
+const API_ID = process.env.API_ID;
 
-app.post('/', (req, res) =>{
-    const fname = req.body.fname
-    const lname = req.body.lname
-    const email = req.body.email
+app.use(express.static("public")); // add static files
+app.use(bodyParser.urlencoded({ extended: true })); // add body parser for request body
 
-    console.log(fname)
-})
+// define a get at the home route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log('Listening')
-})
+// define a post at the home route
+app.post("/", (req, res) => {
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+      },
+    ],
+  };
+  jsondata = JSON.stringify(data);
+  const options = {
+    url: `https://us7.api.mailchimp.com/3.0/lists/${API_ID}`,
+    method: "POST",
+    headers: {
+      Authorization: `tawanda ${API_KEY}`,
+    },
+    body: jsondata,
+  };
+  request(options, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response.statusCode);
+    }
+  });
+});
+
+app.listen(PORT || 3000, () => {
+  console.log(`Listening on ${PORT}`);
+});
